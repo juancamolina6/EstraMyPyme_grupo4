2c48e1c9eff8987da06f7db93fc600c8b80a9d76
Angela Torres <aura@MacBook-Air-de-Aura.local>
2024-07-19 06:04:32 -0500

    valiadacion ok conexion con los dos formulario con jusvalidate

--- src/app/views/registro/registro.component.ts
+++ src/app/views/registro/registro.component.ts
@@ -1,239 +1,77 @@
-import { Component, OnInit } from '@angular/core';
-import JustValidate from 'just-validate';
-import { CommonModule } from '@angular/common';
+import { Component, AfterViewInit, ChangeDetectorRef } from '@angular/core';
+import { ValidationService } from '../../services/validation.service';
 import { FormsModule } from '@angular/forms';
-import { NgModule } from '@angular/core'; 
- 
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-registro',
   standalone: true,
   imports: [CommonModule,FormsModule],
   templateUrl: './registro.component.html',
-  styleUrl: './registro.component.css'
+  styleUrls: ['./registro.component.css']
 })
 
-
-export class RegistroComponent implements OnInit {
-
-
-
-  constructor(){}
-
-  ngOnInit(): void {
-  const validation = new JustValidate("#register", {
-    validateBeforeSubmitting: true,
-    errorLabelStyle: {
-      color: '#dc3545',
-      fontSize: '14px',
-      top: '100%',
-      margin: '5px 0 0 0',
-    },
-    
-  })
+export class RegistroComponent implements AfterViewInit{
   
-  .onValidate(function ({ isValid, fields, groups }) {
-    if (isValid) {
-      console.log("Formulario completado");
-    }
-    if (!isValid) {
-      console.log("Formulario no completado intente de nuevo", fields);
-    }
-  });
+  empresaPasswordVisible: boolean = false; 
+  consultorPasswordVisible: boolean = false;
+  isEmpresaActive: boolean = true; // Controla la visibilidad del formulario de empresa
+  isConsultorActive: boolean = false; // Controla la visibilidad del formulario de consultor
 
- 
-  
-  validation
-  
-  .addField("#name", [
-    
+  password: string = '';
+  passwordVisible: boolean = false;
 
-      {
-        rule: 'required',
-        errorMessage: "*El nombre es obligatorio"
-      },
-      {
-        rule: 'minLength',
-        value: 8,
-        errorMessage: "Minimo 8 caracteres",
-        
-      },
-      {
-        rule: 'maxLength',
-        value: 50,
-        errorMessage: "Maximo 50 caracteres",
-        
-      },
-      
-    ])
-
-  
-    .addField("#email", [
-
-      {
-        rule: 'required',
-        errorMessage: "*campo obligatorio"
-      },
-
-      {
-        rule: 'required',
-        errorMessage: "*campo obligatorio"
-      },
-      {
-        rule: 'email',
-        errorMessage: "Correo no valido",
-        
-      },
-    
-      
-    ])
-
-    .addField("#password", [
-      {
-        rule: 'required',
-        errorMessage: "*campo obligatorio"
-      },
-
-      {
-        rule: 'password',
-        errorMessage: "Usa mayusculas y al menos un numero"
-      },
-      {
-        rule: 'minLength',
-        value: 8,
-        errorMessage: "Minimo 6 caracteres",
-        
-      },
-    
-    
-      
-    ])
-
-    .addField("#namempresa", [
-      {
-        rule: 'required',
-        errorMessage: "*campo obligatorio"
-      },
-      
-      {
-        rule: 'maxLength',
-        value: 25,
-        errorMessage: "Maximo 25 caracteres",
-        
-      },
-    
-    
-      
-    ])
+  constructor(
+    private validationService: ValidationService,
+    private cdr: ChangeDetectorRef
+  ) {}
 
 
-    .addField("#nit", [
-
-      {
-        rule: 'required',
-        errorMessage: "*campo obligatorio"
-      },
-      
-      {
-        rule: 'number',
-        errorMessage: "*solo ingresa numeros",
-        
-      },
-    
-    
-      
-    ])
-
-    .addField("#tipo", [
-      
-      {
-        rule: 'required',
-        errorMessage: "*campo obligatorio"
-      },
-      
-     
-    ])
- 
-    .addField("#tipo2", [
-      
-      {
-        rule: 'required',
-        errorMessage: "*campo obligatorio"
-      },
-      
-     
-    ])
-
-    .addField("#direccion", [
-      
-      {
-        rule: 'required',
-        errorMessage: "*campo obligatorio"
-      },
-      
-     
-    ])
-
-    .addField("#telefono", [
-
-      {
-        rule: 'required',
-        errorMessage: "*campo obligatorio"
-      },
-      
-      {
-        rule: 'number',
-        errorMessage: "*solo ingresa numeros",
-        
-      },
-    
-    
-      
-    ])
-
-    .addField("#acceptTerms", [
-
-      {
-        rule: 'required',
-        errorMessage: "*campo obligatorio"
-      },
-      
-  
-    ])
-
-    .addRequiredGroup('#che', '*campo obligatorio', {
-      tooltip: {
-        position: 'bottom',
-        
-      },
-      
-     
-    })
-    
- 
+  ngAfterViewInit(): void {
+    this.initializeValidators();
   }
 
-
-    password: string = '';
-    passwordVisible: boolean = false;
-  
-    togglePasswordVisibility(): void {
-      this.passwordVisible = !this.passwordVisible;
+  initializeValidators(): void {
+    if (this.isEmpresaActive) {
+      this.validationService.initializeEmpresaValidator();
+    } else if (this.isConsultorActive) {
+      this.validationService.initializeConsultorValidator();
     }
-
-    
-  
-
-
-
-
-
-
-
-
-
+    this.cdr.detectChanges();
   }
-  
+
+  toggleView(view: string): void {
+    if (view === 'empresa') {
+      this.isEmpresaActive = true;
+      this.isConsultorActive = false;
+    } else if (view === 'consultor') {
+      this.isEmpresaActive = false;
+      this.isConsultorActive = true;
+    }
+    this.cdr.detectChanges(); // Detectar los cambios en el DOM
+    setTimeout(() => this.initializeValidators(), 0); // Inicializar las validaciones después de que el DOM se haya actualizado
+  }
+
+ 
+
+
+
+//contraseña 
+
+  togglePasswordVisibility(): void {
+    this.passwordVisible = !this.passwordVisible;
+  }
+
+  togglePassword(type: string): void {
+    if (type === 'empresa') {
+      this.empresaPasswordVisible = !this.empresaPasswordVisible;
+    } else if (type === 'consultor') {
+      this.consultorPasswordVisible = !this.consultorPasswordVisible;
+    }
+  }
+
+ 
+}
 
 
 
