45e463cefd264c0346b4e9527bfd615d9da67f91
Alejox1012 <didianvalencia2@gmail.com>
2024-07-24 20:28:37 -0500

    se agrega landing page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { LandingPageComponent } from './views/landing-page/landing-page.component';
+export const routes: Routes = [
+    {
+        path: '', component: LandingPageComponent
+    },
+    {
+       path:'landingpage', component: LandingPageComponent},
+       //Otras rutas pueden ir aqui
+];
 
-export const routes: Routes = [];
+
