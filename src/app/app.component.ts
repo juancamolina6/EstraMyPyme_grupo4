58172d8b0f0e19b7ed1e7d8135fa7d4c3cf1ee52
juan camilo <juanca2.0@hotmail.com>
2024-07-26 12:41:35 -0500

    se eliminacodigo que no se usa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { CommonModule } from '@angular/common';
-import {RegistroComponent } from './views/registro/registro.component'
-import { LandingPageComponent } from './views/landing-page/landing-page.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,LandingPageComponent,RegistroComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
